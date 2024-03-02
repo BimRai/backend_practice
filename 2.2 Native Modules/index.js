2580867410f9019f00338763be002b1cff5f2987
BimRai <115172913+BimRai@users.noreply.github.com>
2024-03-02 12:25:20 +0000

    Add files via upload

---
+++ 2.2 Native Modules/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// fs.writeFile("message.txt", "Hello from Node.js", (err) => {
+//     if (err) throw err;
+//     console.log("File created!");
+// });
+
+fs.readFile("./message.txt", "utf8", (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
---
+++ 2.3 NPM/index.js
@@ -0,0 +1,7 @@
+//var generateName = require("sillyname");
+//the code below is the import ver of it called ecma
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
